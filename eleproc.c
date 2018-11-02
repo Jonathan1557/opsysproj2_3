dbf88bdca2a41fef115b3bd3de86d32106d02971
cgosterer <cgo11@my.fsu.edu>
2018-11-01 18:44:33 -0700

    finished eleproc.c, added enum and struct before return types of functions, got error otherwise

--- eleproc.c
+++ eleproc.c
@@ -5,6 +5,7 @@
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <linux/time.h>
+#include "elevator.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Displays Information regarding the elevator status, along with status of passengers and the floor");
@@ -47,14 +48,14 @@ int elevator_proc_open (struct inode *sp_inode, struct file *sp_file)
 {
 	printk(KERN_INFO "proc called open\n");
 	read_p = 1;
-	message = kmalloc(sizeof(char) * 1000 , __GFP_RECLAIM | __GFP_IO | __GFP_FS);
+	message = kmalloc(sizeof(char) * 2000 , __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 	if (message == NULL)
 	{
 		printk(KERN_WARNING "elvator_proc_open");
 		return -ENOMEM;
 	}
 
-	strcpy(message, "elevator status: ");
+	strcpy(message, "ELEVATOR STATUS: \n");
 	return 0;
 }
 
@@ -64,10 +65,12 @@ ssize_t elevator_proc_read(struct file *sp_file, char __user *buf, size_t size, 
 		if (read_p)
 			return 0;
 		printk(KERN_INFO "proc called read\n");							// allocating strings to hold the elevator values to output
+
 		movestate = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 		currentfloor = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 		nextfloor  = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 		currentload = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
+
 		floor1w = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 		floor1t = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 		floor2w = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
@@ -89,9 +92,144 @@ ssize_t elevator_proc_read(struct file *sp_file, char __user *buf, size_t size, 
 		floor10w = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
                 floor10t = kmalloc(sizeof(char) * 60, __GFP_RECLAIM | __GFP_IO | __GFP_FS);
 
-		//sprintf(seconds, "%lu", time_spec1.tv_sec);
-		//strcat(seconds,"\n");
-		//strcat(message, seconds );
+
+		strcat(message,"Elevator Movement State:  ");
+		enum ElevatorState es = getState();
+
+		if (es == OFFLINE)
+			strcat(message, "OFFLINE");
+
+		if (es == IDLE)
+			strcat(message, "IDLE");
+
+		if(es == LOADING)
+			strcat(message, "LOADING");
+
+		if (es == UP)
+			strcat(message, "UP");
+
+		if (es == DOWN)
+			strcat(message, "DOWN");
+
+		strcat(message, "\n");
+
+		strcat(message, "Current Floor of Elevator: ");
+		sprintf(currentfloor, "%d", getFloor());
+		strcat(message, currentfloor);
+		strcat(message, "\n");
+
+		strcat(message, "Next floor to service: ");
+		sprintf(nextfloor, "%d", getDest());
+		strcat(message, nextfloor);
+		strcat(message, "\n");
+
+		strcat(message, "Elevator Current Load: ");
+		sprintf(currentload, "%d", getWeight());
+		strcat(message, currentload);
+		strcat(message, "\n");
+
+
+		strcat(message, "Floor 1 Total serviced: ");
+		sprintf(floor1t, "%d", serviced[0] );		// putting total serviced into their strings for output in proc file
+		strcat(message,floor1t);
+		strcat(message,"\n");
+
+		strcat(message, "Floor 2 Total serviced: ");
+                sprintf(floor2t, "%d", serviced[1] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor2t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 3 Total serviced: ");
+                sprintf(floor3t, "%d", serviced[2] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor3t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 4 Total serviced: ");
+                sprintf(floor4t, "%d", serviced[3] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor4t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 5 Total serviced: ");
+                sprintf(floor5t, "%d", serviced[4] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor5t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 6 Total serviced: ");
+                sprintf(floor6t, "%d", serviced[5] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor6t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 7 Total serviced: ");
+                sprintf(floor7t, "%d", serviced[6] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor7t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 8 Total serviced: ");
+                sprintf(floor8t, "%d", serviced[7] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor8t);
+                strcat(message,"\n");
+
+
+		strcat(message, "Floor 9 Total serviced: ");
+                sprintf(floor9t, "%d", serviced[8] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor9t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 10 Total serviced: ");
+                sprintf(floor10t, "%d", serviced[9] );           // putting total serviced into their strings for output in proc file
+                strcat(message,floor10t);
+                strcat(message,"\n");
+
+		strcat(message, "Floor 1 Number Waiting Load: ");
+		sprintf(floor1w, "%d", getWaitingLoad(1)  );           // putting waiting people load into their strings for output in proc file
+		strcat(message, floor1w);
+		strcat(message, "\n");
+
+		strcat(message, "Floor 2 Number Waiting Load: ");
+                sprintf(floor2w, "%d", getWaitingLoad(2)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor2w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 3 Number Waiting Load: ");
+                sprintf(floor3w, "%d", getWaitingLoad(3)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor3w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 4 Number Waiting Load: ");
+                sprintf(floor4w, "%d", getWaitingLoad(4)  );           // putting waiting people into their strings for output in proc file
+                strcat(message, floor4w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 5 Number Waiting Load: ");
+                sprintf(floor5w, "%d", getWaitingLoad(5)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor5w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 6 Number Waiting Load: ");
+                sprintf(floor6w, "%d", getWaitingLoad(6)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor6w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 7 Number Waiting Load: ");
+                sprintf(floor7w, "%d", getWaitingLoad(7)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor7w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 8 Number Waiting Load: ");
+                sprintf(floor8w, "%d", getWaitingLoad(8)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor8w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 9 Number Waiting Load: ");
+                sprintf(floor9w, "%d", getWaitingLoad(9)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor9w);
+                strcat(message, "\n");
+
+		strcat(message, "Floor 10 Number Waiting Load: ");
+                sprintf(floor10w, "%d", getWaitingLoad(10)  );           // putting waiting people load into their strings for output in proc file
+                strcat(message, floor10w);
+                strcat(message, "\n");
+
 		int len = strlen(message);
 		copy_to_user(buf, message, len);			// actually prints to proc file, this will work for elevator
 		return len;
@@ -99,7 +237,35 @@ ssize_t elevator_proc_read(struct file *sp_file, char __user *buf, size_t size, 
 
 int elevator_proc_release(struct inode *sp_inode, struct file *sp_file)
 {
-	printk(KERN_INFO "proc called release\n");
+	printk(KERN_INFO "proc called release\n");			// free all strings here
+
+	kfree(movestate);
+        kfree(currentfloor);
+        kfree(nextfloor);
+        kfree(currentload);
+
+	kfree(floor1w);
+	kfree(floor2w);
+	kfree(floor3w);
+	kfree(floor4w);
+	kfree(floor5w);
+	kfree(floor6w);
+	kfree(floor7w);
+	kfree(floor8w);
+	kfree(floor9w);
+	kfree(floor10w);
+
+	kfree(floor1t);
+        kfree(floor2t);
+        kfree(floor3t);
+        kfree(floor4t);
+        kfree(floor5t);
+        kfree(floor6t);
+        kfree(floor7t);
+        kfree(floor8t);
+        kfree(floor9t);
+        kfree(floor10t);
+
 	kfree(message);
 	return 0;
 }
--- elevator.h
+++ elevator.h
@@ -19,34 +19,39 @@ enum ElevatorState{
 
 //Integers that store the number of passengers serviced to a specific floor
 //Needed for proc file
-int serviced[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+int serviced[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 //Function that elevator can call to check each floor when scheduling
 //Returns the passenger next in line without removing them
-Passenger checkWaiter(int floor);
+struct Passenger checkWaiter(int floor);
 
 //Function that elevator can call to load a passenger
 //Returns the passenger next in line and removes them from line
 //The elevator must add the returned passenger to itself
-Passenger loadWaiter(int floor);
+struct Passenger loadWaiter(int floor);
 
 //Returns number of passengers on given floor
 int numPassengers(int floor);
 
 //Returns current state of elevator
-ElevatorState getState(void);
+enum ElevatorState getState(void)
+{return OFFLINE;}
 
 //Returns current floor of elevator
-int getFloor(void);
+int getFloor(void)
+{return 1;}
 
 //Returns current destination of elevator
-int getDest(void);
+int getDest(void)
+{return 1;}
 
 //Returns current weight held by elevator
-int getWeight(void);
+int getWeight(void)
+{return 123;}
 
 //Returns number of passengers held by elevator
 int getCapacity(void);
 
-// returns weight of all passengers waiting on a given floor
-int getWaitingLoad(int floor);
+// returns weight of all passengers waiting ona given floor
+int getWaitingLoad(int floor)
+{return 321;}
